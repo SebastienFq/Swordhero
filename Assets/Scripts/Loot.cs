3b691a96e7da9b16fa5bc9cbc76e121616171367
Sebastien Fouque <sebastienf@voodoo.io>
2022-09-13 22:03:21 +0200

    + loots wip

--- Assets/Scripts/Loot.cs
+++ Assets/Scripts/Loot.cs
@@ -10,12 +10,18 @@ public class Loot : MonoBehaviour
         CURRENCY
     }
 
+    [SerializeField] private Transform m_ItemSlot;
+
     private Item m_Item;
     private LootType m_LootType;
 
     public void Init(Item _Item, object _LootType)
     {
         m_Item = _Item;
-        //m_LootType = _LootType;
+
+        _Item.transform.SetParent(m_ItemSlot);
+        _Item.transform.localRotation = Quaternion.Euler(90, 0, 0);
+        _Item.transform.localPosition = Vector3.zero;
+        _Item.transform.localScale = Vector3.one;
     }
 }
