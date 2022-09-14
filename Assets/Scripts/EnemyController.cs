451909dc8490c874712afe7453d8c96702a194d9
Sebastien Fouque <sebastienf@voodoo.io>
2022-09-14 18:57:12 +0200

    + AI wip ! c'est pété

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,19 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
+    private const float c_RecalculatePathDelay = 0.2f;
+
     [Header("References")]
-    //[SerializeField] private Rigidbody m_Body;
+    [SerializeField] private Renderer m_Renderer;
+    [SerializeField] private Rigidbody m_Body;
     [SerializeField] private Health m_Health;
 
     [Header("Settings")]
     [SerializeField] private int m_TotalHealth = 100;
     [SerializeField] private float m_MoveSpeed = 10;
+    [SerializeField] private float m_MinMoveDistance = 2;
+    [SerializeField] private float m_OffsetMoveDistance = 4;
+    [SerializeField] private float m_AttackDuration = 8;
+    [SerializeField] private float m_AttackDurationOffset = 4;
+    [SerializeField] private float m_WanderDuration = 8;
+    [SerializeField] private float m_WanderDurationOffset = 4;
+    [SerializeField] private float m_WanderPauseDuration = 2;
+    [SerializeField] private float m_WanderPauseDurationOffset = 2;
 
     //private IEnumerator moveRoutine;
     private float m_DistanceFromPlayer = 0;
+    private bool m_IsAttacking = false;
+    private bool m_IsWandering = false;
+    private float m_TimerRecalculatePath;
+    private PlayerController m_Player;
+    private NavMeshPath path;
+    private int m_NextTarget;
+    private Vector3 m_Destination;
+    private Vector3 m_MovingDirection;
+    private float m_TimerAction;
+    private float m_TimerPause;
 
     public static Action<EnemyController> onDeath;
     public Health Health => m_Health;
@@ -42,10 +65,111 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (FSMManager.Instance.CurrentPhase != GamePhase.GAME)
+            return;
+
+        if (m_IsAttacking)
+        {
+            m_TimerRecalculatePath -= Time.deltaTime;
+
+            if (m_TimerRecalculatePath <= 0)
+            {
+                NavMesh.CalculatePath(transform.position, m_Player.transform.position, NavMesh.AllAreas, path);
+                m_TimerRecalculatePath = c_RecalculatePathDelay;
+            }
+
+            Vector3 lastPoint = transform.position;
+
+            for (int i = 0; i < path.corners.Length; i++)
+            {
+                Debug.DrawLine(lastPoint, path.corners[i], Color.cyan, Time.deltaTime);
+                lastPoint = path.corners[i];
+            }
+
+            Debug.DrawLine(lastPoint, m_Player.transform.position, Color.cyan, Time.deltaTime);
+
+            Vector3 dir = ((path.corners.Length > 1 ? path.corners[1] : m_Player.transform.position) - transform.position).normalized;
+            dir.y = 0;
+
+            SetMovingDirection(dir);
+
+            m_TimerAction -= Time.deltaTime;
+
+            if(m_TimerAction <= 0)
+            {
+                StopAttack();
+            }
+        }
+        else
+        {
+            if(m_IsWandering)
+            {
+                if (path.corners.Length == 0) return;
+
+                Vector3 lastPoint = path.corners[0];
+
+                for (int i = 0; i < path.corners.Length; i++)
+                {
+                    Debug.DrawLine(lastPoint, path.corners[i], Color.cyan, Time.deltaTime);
+                    lastPoint = path.corners[i];
+                }
+
+                if (path.corners.Length > 1)
+                {
+                    if (Vector3.Distance(transform.position, path.corners[m_NextTarget]) < 0.5f)
+                    {
+                        if (m_NextTarget + 1 >= path.corners.Length)
+                        {
+                            ReachDestination();
+                            return;
+                        }
+                        else
+                        {
+                            m_NextTarget++;
+                        }
+                    }
+                }
+
+                Vector3 dir = ((path.corners.Length > 1 ? path.corners[m_NextTarget] : m_Player.transform.position) - transform.position).normalized;
+                dir.y = 0;
+
+                SetMovingDirection(dir);             
+            }
+            else
+            {
+                m_TimerPause -= Time.deltaTime;
+
+                if (m_TimerPause <= 0)
+                {
+                    m_IsWandering = true;
+                }
+            }
+
+            m_TimerAction -= Time.deltaTime;
+
+            if(m_TimerAction <= 0)
+            {
+                StartAttack();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_IsAttacking || m_IsWandering)
+        {
+            m_Body.MovePosition(m_Body.position + m_MovingDirection * Time.fixedDeltaTime * m_MoveSpeed);
+        }
+    }
+
     public void Init()
     {
+        path = new NavMeshPath();
         m_Health.Init(m_TotalHealth);
-        //GoTo(FindRandomPointInMapRange(), () => { });
+        m_Player = UnitManager.Instance.Player;
+        StopAttack();
     }
 
     private void OnGamePhaseStarted(GamePhase _Phase)
@@ -57,37 +181,56 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    private bool FindRandomDestination()
+    {
+        Vector2 c = UnityEngine.Random.insideUnitCircle;
+        var randomDirection = new Vector3(c.x, 0, c.y);
+
+        NavMeshHit hit;
+
+        if (!NavMesh.SamplePosition(transform.position + randomDirection * (m_MinMoveDistance + m_OffsetMoveDistance * UnityEngine.Random.value), out hit, m_MinMoveDistance + m_OffsetMoveDistance, NavMesh.AllAreas))
+        {
+            return false;
+        }
+
+        m_Destination = hit.position;
+        NavMesh.CalculatePath(transform.position, m_Destination, NavMesh.AllAreas, path);
+        m_NextTarget = 1;
+        ExtensionMethods.DrawDot(m_Destination, 1, Color.magenta, 5f);
+        return true;
+    }
+
+    private void StartAttack()
+    {
+        m_IsAttacking = true;
+        m_IsWandering = false;
+        m_TimerAction = m_AttackDuration + m_AttackDurationOffset * Random.value;
+        m_Renderer.material.SetColor("_EmissioNnColor", Color.red);
+    }
+
+    private void StopAttack()
+    {
+        m_IsAttacking = false;
+        m_IsWandering = false;
+        m_TimerAction = m_WanderDuration + m_WanderDurationOffset * Random.value;
+        m_Renderer.material.SetColor("_EmissioNnColor", Color.black);
+    }
+
+    private void ReachDestination()
+    {
+        m_TimerPause = m_WanderPauseDuration = m_WanderPauseDurationOffset * Random.value;
+        m_IsWandering = false;       
+    }
+
+    private void SetMovingDirection(Vector3 direction)
+    {
+        m_MovingDirection = direction;
+    }
 
     public void SetDistanceFromPlayer(float _Dist)
     {
         m_DistanceFromPlayer = _Dist;
     }
-
-    /*private void GoTo(Vector3 _Destination, Action _Callback)
-    {
-        if (moveRoutine != null) StopCoroutine(moveRoutine);
-        moveRoutine = MoveRoutine(_Destination, _Callback);
-        StartCoroutine(moveRoutine);
-    }*/
-
-    /*private IEnumerator MoveRoutine(Vector3 _Destination, Action _Callback)
-    {
-        float timer = 0;
-        Vector3 start = m_Body.position;
-        Vector3 end = _Destination;
-        float distance = Vector3.Distance(start, end);
-
-        while (timer < 1)
-        {
-            m_Body.MovePosition(Vector3.Lerp(start, end, timer));
-            timer += Time.fixedDeltaTime / (distance / m_MoveSpeed);
-            yield return new WaitForFixedUpdate();
-        }
-
-        m_Body.MovePosition(end);
-
-        _Callback();
-    }*/
 
     public void Destroy()
     {
